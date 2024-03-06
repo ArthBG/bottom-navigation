ad0bc0c3d93094a0129bc15cdaf99b737aab903d
Arthur Borges <arthurborges.senai564@gmail.com>
2024-03-06 08:17:33 -0300

    refactor: project structure

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import styles from './style'
+const Title = () => {
+  return (
+    <View>
+      <Text style={styles.titulo}>Title</Text>
+    </View>
+  )
+}
+
+export default Title
