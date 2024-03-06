5beaa206f4643099c732cfb7f27847e330a9a2d5
Arthur Borges <arthurborges.senai564@gmail.com>
2024-03-06 09:09:56 -0300

    Feat: Add tab bar icons and labels to navigation

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from './src/screens/Home';
 import Profile from './src/screens/Profile';
 import Contact from './src/screens/Contact';
@@ -11,9 +12,27 @@ export default function App() {
   return (
    <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profile" component={Profile} />
-        <Tab.Screen name="Contact" component={Contact} />
+        <Tab.Screen name="Home" component={Home} 
+        options={{
+          tabBarLabel: 'Início',
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="home" color={452831} size={32} />
+          ),
+        }}/>
+        <Tab.Screen name="Profile" component={Profile} 
+         options={{
+          tabBarLabel: 'Perfil',
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="account" color={452831} size={32} />
+          ),
+        }}/>
+        <Tab.Screen name="Contact" component={Contact} 
+         options={{
+          tabBarLabel: 'Contato',
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="phone" color={452831} size={32} />
+          ),
+        }}/>
       </Tab.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
